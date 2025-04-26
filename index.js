e2cc70950389f8d1aa50e5a96e75ed687456ee0a
mallick1126 <rahulmallick1126@gmail.com>
2025-04-26 12:56:16 +0530

    port changes

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import urlShortener from './routes/urlshortener.js';
-import {connectDB} from './db/dbconfig.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import urlShortener from "./routes/urlshortener.js";
+import { connectDB } from "./db/dbconfig.js";
+import cors from "cors";
 
 dotenv.config();
 
 const port = process.env.PORT || 9600;
 
-
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(cors());
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
-app.use('/', urlShortener);
+app.use("/", urlShortener);
 connectDB();
 
-
-app.listen(port, ()=>{
-    console.log(`server started on port ${port}`);
-})
+app.listen(9600, "0.0.0.0", () => {
+  console.log("Server started on port 9600");
+});
